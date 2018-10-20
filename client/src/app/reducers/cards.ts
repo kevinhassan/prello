cf76d03d0f335a06e96f3fd2030dddd57b3802fa
Clément Roig <clm.roig@gmail.com>
2018-10-20 16:25:02 +0200

    Delete ADD_CARD Action (doesn't exist)

--- client/src/app/reducers/cards.ts
+++ client/src/app/reducers/cards.ts
@@ -20,21 +20,6 @@ const initialState: RootState.CardState = [
 
 export const cardReducer = handleActions<RootState.CardState, CardModel>(
     {
-        [CardActions.Type.ADD_CARD]: (state, action) => {
-            if (action.payload && action.payload.description) {
-                return [
-                    {
-                        id: state.reduce((max, card) => Math.max(card.id || 1, max), 0) + 1,
-                        description: action.payload.description,
-                        isArchived: false,
-                        dueDate: new Date()
-                    },
-                    ...state,
-                ];
-            } else {
-                return state;
-            }
-        },
         [CardActions.Type.DELETE_CARD]: (state, action) => {
             return state.filter((card) => card.id !== (action.payload as any));
         }
