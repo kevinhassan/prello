66ae82f8bd44c300415953c7ad4745885723d738
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2018-10-22 17:49:23 +0200

    add: attachment mongoose model
    Signed-off-by: kevinhassan <kevin.hassan@etu.umontpellier.fr>

---
+++ server/src/models/Attachment.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import Card, { CardModel } from "./Card";
+
+export type AttachmentModel = mongoose.Document & {
+    name: string,
+    url: string,
+    card: CardModel
+};
+
+const attachmentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    url:  { type: String, require: true },
+    card: { type: Card, required: true }
+}, { timestamps: true });
+
+const Attachment = mongoose.model("Attachment", attachmentSchema, "Attachments");
+export default Attachment;
