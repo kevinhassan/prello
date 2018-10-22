6a8c154da9317218a33e403c1c3c2bf5159f74a2
Clément Roig <clm.roig@gmail.com>
2018-10-22 16:40:52 +0200

    new linter indet + actions compliant

--- client/src/actions/cards.js
+++ client/src/actions/cards.js
@@ -1,66 +1,53 @@
 import { displayLoadingModal, hideLoadingModal } from './modal';
-import { fetchPrelloAPI, DELETE } from '../helpers/fetchPrelloAPI'
+import { fetchPrelloAPI, DELETE } from '../helpers/fetchPrelloAPI';
 
 // ========================
 
-export const DELETE_CARD = 'cards/DELETE_CARD'
-export const DELETE_CARD_WITH_DELAY_STARTED = 'cards/DELETE_CARD_WITH_DELAY_STARTED'
-export const DELETE_CARD_WITH_DELAY_FAILURE = 'cards/DELETE_CARD_WITH_DELAY_FAILURE'
-export const CREATE_CARD = 'cards/CREATE_CARD'
-export const DELETE_CARD_WITH_DELAY_SUCCESS = 'cards/DELETE_CARD_WITH_DELAY_SUCCESS'
-export const DELETE_CARD_WITH_DELAY_FAILURE = 'cards/DELETE_CARD_WITH_DELAY_FAILURE'
-export const CREATE_CARD = 'cards/CREATE_CARD'
+export const DELETE_CARD = 'cards/DELETE_CARD';
+export const DELETE_CARD_WITH_DELAY_STARTED = 'cards/DELETE_CARD_WITH_DELAY_STARTED';
+export const DELETE_CARD_WITH_DELAY_SUCCESS = 'cards/DELETE_CARD_WITH_DELAY_SUCCESS';
+export const DELETE_CARD_WITH_DELAY_FAILURE = 'cards/DELETE_CARD_WITH_DELAY_FAILURE';
+export const CREATE_CARD = 'cards/CREATE_CARD';
 
-export const deleteCard = cardId => (dispatch) => { dispatch(deleteCardAction(cardId)); };
 export const deleteCardAction = cardId => ({
-  type: DELETE_CARD,
-  payload: {
-    id: cardId,
-  },
+    type: DELETE_CARD,
+    payload: {
+        id: cardId,
+    },
 });
+export const deleteCard = cardId => (dispatch) => { dispatch(deleteCardAction(cardId)); };
 
-export const deleteCardWithDelay = (cardId) => {
-    return dispatch => {
-        dispatch(displayLoadingModal())
-        dispatch(deleteCardWithDelayStartedAction())
-        setTimeout(() => {
-            fetchPrelloAPI('/cards', {id: cardId}, DELETE)
-            .then(function(res) {
-                dispatch(deleteCardWithDelaySuccessAction(cardId))
-                dispatch(hideLoadingModal())
+export const deleteCardWithDelayStartedAction = () => ({ type: DELETE_CARD_WITH_DELAY_STARTED });
+export const deleteCardWithDelaySuccessAction = (cardId, res) => ({
+    type: DELETE_CARD_WITH_DELAY_SUCCESS,
+    payload: {
+        id: cardId,
+        res,
+    },
+});
+export const deleteCardWithDelayFailureAction = (cardId, error) => ({
+    type: DELETE_CARD_WITH_DELAY_SUCCESS,
+    payload: {
+        id: cardId,
+        error,
+    },
+});
+export const deleteCardWithDelay = cardId => (dispatch) => {
+    dispatch(displayLoadingModal());
+    dispatch(deleteCardWithDelayStartedAction());
+    setTimeout(() => {
+        fetchPrelloAPI('/cards', { id: cardId }, DELETE)
+            .then(() => {
+                dispatch(deleteCardWithDelaySuccessAction(cardId));
+                dispatch(hideLoadingModal());
             })
-            .catch(function(error) {
-                dispatch(deleteCardWithDelayFailureAction(cardId, error))
-            })
-        }, 2000)
-    }
-}
-export const deleteCardWithDelayStartedAction = () => {
-    return { type: DELETE_CARD_WITH_DELAY_STARTED }
-}
-export const deleteCardWithDelaySuccessAction = (cardId, res) => {
-    return {
-        type: DELETE_CARD_WITH_DELAY_SUCCESS,
-        payload: {
-            id: cardId,
-            res: res
-        }
-    }
-}
-export const deleteCardWithDelayFailureAction = (cardId, error) => {
-    return {
-        type: DELETE_CARD_WITH_DELAY_SUCCESS,
-        payload: {
-            id: cardId,
-            error: error
-        }
-    }
-}
+            .catch((error) => {
+                dispatch(deleteCardWithDelayFailureAction(cardId, error));
+            });
+    }, 2000);
+};
 
-
-export const createCard = () => { return dispatch => dispatch(createCardAction()) }
-export const createCardAction = () => {
-    return {
-        type: CREATE_CARD
-    }
-}
+export const createCardAction = () => ({
+    type: CREATE_CARD,
+});
+export const createCard = () => dispatch => dispatch(createCardAction());
--- client/src/actions/modal.js
+++ client/src/actions/modal.js
@@ -1,12 +1,12 @@
 export const DISPLAY_LOADING_MODAL = 'modal/DISPLAY_LOADING_MODAL';
 export const HIDE_LOADING_MODAL = 'modal/HIDE_LOADING_MODAL';
 
-export const displayLoadingModal = () => (dispatch) => { dispatch(displayLoadingModalAction()); };
 export const displayLoadingModalAction = () => ({
-  type: DISPLAY_LOADING_MODAL,
+    type: DISPLAY_LOADING_MODAL,
 });
+export const displayLoadingModal = () => (dispatch) => { dispatch(displayLoadingModalAction()); };
 
-export const hideLoadingModal = () => (dispatch) => { dispatch(hideLoadingModalAction()); };
 export const hideLoadingModalAction = () => ({
-  type: HIDE_LOADING_MODAL,
+    type: HIDE_LOADING_MODAL,
 });
+export const hideLoadingModal = () => (dispatch) => { dispatch(hideLoadingModalAction()); };
