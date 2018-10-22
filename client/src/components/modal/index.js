8ec04cf5af89e05228478c31e666eef7228c1216
Clément Roig <clm.roig@gmail.com>
2018-10-22 17:32:27 +0200

    linteeeer

--- client/src/components/modal/index.js
+++ client/src/components/modal/index.js
@@ -1,44 +1,45 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-
+import PropTypes from 'prop-types';
 import { displayLoadingModal } from '../../actions/modal';
 import './style.css';
 import spinner from '../../assets/spinner.gif';
 
 class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    if (this.props.isModalOpen) {
-      return (
-        <div className="loadingModal">
-          <span className="modalContent">
-            <img src={spinner} alt="Loading spinner" width="50" />
-          </span>
-        </div>
-      );
+    render() {
+        const { isModalOpen } = this.props;
+        if (isModalOpen) {
+            return (
+                <div className="loadingModal">
+                    <span className="modalContent">
+                        <img src={spinner} alt="Loading spinner" width="50" />
+                    </span>
+                </div>
+            );
+        }
+        return ('');
     }
-    return ('');
-  }
 }
+
+Modal.propTypes = {
+    isModalOpen: PropTypes.bool.isRequired,
+};
 
 // Put info from the store state in props
 const mapStateToProps = ({ modal }) => ({
-  isModalOpen: modal.isModalOpen,
+    isModalOpen: modal.isModalOpen,
 });
 
 // Put actions in props
 const mapDispatchToProps = dispatch => bindActionCreators(
-  {
-    displayLoadingModal,
-  },
-  dispatch,
+    {
+        displayLoadingModal,
+    },
+    dispatch,
 );
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
+    mapStateToProps,
+    mapDispatchToProps,
 )(Modal);
