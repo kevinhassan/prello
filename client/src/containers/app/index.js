a087a7432f722258d41109e8fda9567aa3bafc59
Clément Roig <clm.roig@gmail.com>
2018-10-23 19:22:08 +0200

    Feature done (simple empty header with some buttons)

--- client/src/containers/app/index.js
+++ client/src/containers/app/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import About from '../about';
 import Board from '../board';
 import Modal from '../../components/modal';
+import Header from '../header';
+
 import './style.css';
 
 const App = () => (
     <div>
-        <header>
-            <div>
-                <Link className="link" to="/">Board </Link>
-                <Link className="link" to="/about-us">About</Link>
-            </div>
-        </header>
-
+        <Header />
         <Modal />
-
         <main>
             <Route exact path="/" component={Board} />
             <Route exact path="/about-us" component={About} />
---
+++ client/src/containers/header/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+// ===== Actions
+
+// ===== Others
+import './style.css';
+import { push } from 'connected-react-router';
+
+class Header extends React.Component {
+    render() {
+        const { goHome, goAbout } = this.props;
+        const element = (
+            <div className="header-bar">
+                <button className="btn btn-header" type="button" onClick={goHome}><i className="fas fa-home" /></button>
+                <span className="prello-logo" onClick={goHome} onKeyPress={goHome} role="link" tabIndex={0}>Prello</span>
+                <div>
+                    <button className="btn btn-header" type="button"><i className="fas fa-bell" /></button>
+                    <button className="btn btn-header" type="button" onClick={goAbout}><i className="fas fa-palette" /></button>
+                </div>
+            </div>
+        );
+        return element;
+    }
+}
+Header.propTypes = {
+    goHome: PropTypes.func.isRequired,
+    goAbout: PropTypes.func.isRequired,
+};
+
+// Put info from the store state in props (None)
+const mapStateToProps = () => ({});
+
+// Put actions in props
+const mapDispatchToProps = dispatch => bindActionCreators(
+    {
+        goHome: () => push('/'),
+        goAbout: () => push('/about-us'),
+    }, dispatch,
+);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Header);
