a4d84bcdb051d2029b5eadd47fed92f39a4da086
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2018-10-24 12:44:08 +0200

    edit: app & server transform to class
    Signed-off-by: kevinhassan <kevin.hassan@etu.umontpellier.fr>

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,37 @@
 import errorHandler from "errorhandler";
 
 import app from "./app";
+import express from "express";
 
 /**
  * Error Handler. Provides full stack - remove for production
  */
-app.use(errorHandler());
 
 /**
  * Start Express server.
  */
-const server = app.listen(app.get("port"), () => {
-  console.log(
-    "  App is running at http://localhost:%d in %s mode",
-    app.get("port"),
-    app.get("env")
-  );
-  console.log("  Press CTRL-C to stop\n");
-});
 
-export default server;
+class Server {
+  public app: express.Application;
+  constructor() {
+    this.app = app;
+    this.config();
+  }
+  public run() {
+    this.app.listen(app.get("port"), () => {
+      console.log(
+        "  App is running at http://localhost:%d in %s mode",
+        app.get("port"),
+        app.get("env")
+      );
+      console.log("  Press CTRL-C to stop\n");
+    });
+  }
+
+  private config() {
+    // remove for production
+    this.app.use(errorHandler());
+  }
+}
+
+new Server().run();
