baf53628480e6e7eb55afd6157b0c9daebf10ddc
Clément Roig <clm.roig@gmail.com>
2018-10-25 15:10:10 +0200

    es-lint fix for redux dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -12,7 +12,9 @@ const enhancers = [];
 const middleware = [thunk, routerMiddleware(history), logger];
 
 if (process.env.NODE_ENV === 'development') {
+    /* eslint-disable no-underscore-dangle */
     const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+    /* eslint-enable */
 
     if (typeof devToolsExtension === 'function') {
         enhancers.push(devToolsExtension());
