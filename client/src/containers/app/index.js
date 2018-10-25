cff63eac13103bd22a1a65436e26593a10e47712
Cyprien Legrand <cyprien_legrand@hotmail.fr>
2018-10-25 19:02:00 +0200

    Add sign in/sign up forms

--- client/src/containers/app/index.js
+++ client/src/containers/app/index.js
@@ -5,6 +5,8 @@ import BoardComp from '../BoardComp';
 import Modal from '../../components/modal';
 import Header from '../header';
 import UserComp from '../UserComp';
+import SignUpComp from '../SignUpComp';
+import SignInComp from '../SignInComp';
 
 import './style.css';
 
@@ -14,6 +16,8 @@ const App = () => (
         <Modal />
         <main style={{ marginTop: '50px' }}>
             <Route exact path="/" component={BoardComp} />
+            <Route exact path="/login" component={SignInComp} />
+            <Route exact path="/signup" component={SignUpComp} />
             <Route exact path="/profile" component={UserComp} />
             <Route exact path="/about-us" component={About} />
         </main>
