c47e8d00828ccf850cf38a7b482a6590d2891384
Clément Roig <clm.roig@gmail.com>
2018-10-26 22:22:05 +0200

    State updated  correctly after DnD

--- client/src/actions/board.js
+++ client/src/actions/board.js
@@ -75,4 +75,5 @@ export const updateListsIndexes = (boardId, newLists) => (dispatch) => {
         .catch((error) => {
             dispatch(updateListsIndexesFailureAction(error.message));
         });
+*/
 };
