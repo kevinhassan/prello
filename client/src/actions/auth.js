aabeda3995c9b388e37e124e173e248bad805148
Clément Roig <clm.roig@gmail.com>
2018-10-26 22:51:52 +0200

    Clean actions

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,17 +1,8 @@
 import * as APIFetch from '../helpers/APIFetch';
 
-export const CLASSIC_SIGN_IN = 'auth/CLASSIC_SIGN_IN';
 export const CLASSIC_SIGN_IN_STARTED = 'auth/CLASSIC_SIGN_IN_STARTED';
 export const CLASSIC_SIGN_IN_FAILURE = 'auth/CLASSIC_SIGN_IN_FAILURE';
 export const CLASSIC_SIGN_IN_SUCCESS = 'auth/CLASSIC_SIGN_IN_SUCCESS';
-
-export const classicSignInAction = (email, password) => ({
-    type: CLASSIC_SIGN_IN,
-    payload: {
-        email,
-        password,
-    },
-});
 
 export const classicSignInStarted = () => ({ type: CLASSIC_SIGN_IN_STARTED });
 export const classicSignInFailure = error => ({
--- client/src/actions/board.js
+++ client/src/actions/board.js
@@ -2,11 +2,11 @@ import { displayLoadingModal, hideLoadingModal } from './modal';
 
 // ========================
 
-export const FETCH_BOARD = 'board/FETCH_BOARD';
-export const FETCH_BOARD_FAILURE = 'board/FETCH_BOARD_FAILURE';
 export const FETCH_BOARD_STARTED = 'board/FETCH_BOARD_STARTED';
+export const FETCH_BOARD_FAILURE = 'board/FETCH_BOARD_FAILURE';
 export const FETCH_BOARD_SUCCESS = 'board/FETCH_BOARD_SUCCESS';
 
+export const fetchBoardStartedAction = () => ({ type: FETCH_BOARD_STARTED });
 export const fetchBoardFailureAction = (boardId, error) => ({
     type: FETCH_BOARD_FAILURE,
     payload: {
@@ -14,7 +14,6 @@ export const fetchBoardFailureAction = (boardId, error) => ({
         error,
     },
 });
-export const fetchBoardStartedAction = () => ({ type: FETCH_BOARD_STARTED });
 export const fetchBoardSuccessAction = (boardId, res) => ({
     type: FETCH_BOARD_SUCCESS,
     payload: {
@@ -23,12 +22,6 @@ export const fetchBoardSuccessAction = (boardId, res) => ({
     },
 });
 
-export const fetchBoardAction = boardId => ({
-    type: FETCH_BOARD,
-    payload: {
-        id: boardId,
-    },
-});
 export const fetchBoard = boardId => (dispatch) => {
     dispatch(displayLoadingModal());
     dispatch(fetchBoardStartedAction());
