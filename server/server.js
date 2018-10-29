5f03acb9636b70e40a8ccc97f822bf03c06f167d
Clément Roig <clm.roig@gmail.com>
2018-10-29 21:37:12 +0100

    Server : fix app name + indent

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ const app = require('./app');
  * Start Express server.
  */
 app.listen(app.get('port'), () => {
-  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
-  console.log('  Press CTRL-C to stop\n');
+    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+    console.log('  Press CTRL-C to stop\n');
 });
