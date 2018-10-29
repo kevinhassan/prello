0d85e41af8bcfe331868b554ab9159d496ca39e2
Clément Roig <clm.roig@gmail.com>
2018-10-29 13:12:17 +0100

    Fix front tests (delete obsolete ones)

--- client/src/reducers/cards.test.js
+++ client/src/reducers/cards.test.js
@@ -8,26 +8,3 @@ describe('cards reducer', () => {
         // Initial state
     });
 });
-
-describe('cards/DELETE_CARD reducer', () => {
-    it('should delete the card with id specified', () => {
-        const deleteAction = actions.deleteCardAction(1);
-        const finalState = cardsReducer(undefined, deleteAction);
-        expect(finalState.cards.length).toEqual(4);
-    });
-
-    it('should set an appropriate error if id not found', () => {
-        const deleteAction = actions.deleteCardAction(42);
-        const finalState = cardsReducer(undefined, deleteAction);
-        expect(finalState.cards.length).toEqual(5);
-        expect(finalState.error.toLowerCase()).toEqual(expect.stringContaining('not found'));
-    });
-});
-
-describe('cards/DELETE_CARD_WITH_DELAY_STARTED reducer', () => {
-    it('should set isLoading to true', () => {
-        const action = actions.deleteCardWithDelayStartedAction();
-        const finalState = cardsReducer(undefined, action);
-        expect(finalState.isLoading).toBe(true);
-    });
-});
