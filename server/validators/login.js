d943c62f9e03fd08bac38df03d82039d92c65dd1
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2018-10-30 14:26:51 +0100

    add: type checks on validators
    Signed-off-by: kevinhassan <kevin.hassan@etu.umontpellier.fr>

--- server/validators/login.js
+++ server/validators/login.js
@@ -2,9 +2,14 @@ const { check } = require('express-validator/check');
 
 module.exports = [
     check('password')
+        .escape()
         .not().isEmpty()
+        .withMessage('Password is required')
+        .isString()
         .withMessage('Password is required'),
     check('email')
+        .escape()
+        .trim()
         .isEmail()
         .withMessage('That email doesn‘t look right')
 ];
--- server/validators/register.js
+++ server/validators/register.js
@@ -3,13 +3,21 @@ const { check } = require('express-validator/check');
 module.exports = [
     check('fullname')
         .not().isEmpty()
+        .escape()
+        .withMessage('Fullname is required')
+        .isString('Fullname need to be string')
         .withMessage('Fullname is required'),
     check('email')
+        .trim()
+        .escape()
         .isEmail()
         .withMessage('That email doesn‘t look right'),
     check('password')
         .not().isEmpty()
+        .escape()
         .withMessage('Password is required')
+        .isString()
+        .withMessage('Password must be a string')
         .isLength({ min: 5 })
         .withMessage('Password need to have at least 5 characters')
 ];
