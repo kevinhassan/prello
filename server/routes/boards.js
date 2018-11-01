f38705723d60da3dcd26703a3eb9a91598a4c647
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2018-11-01 14:16:50 +0100

    doc: board swagger documentation

--- server/routes/boards.js
+++ server/routes/boards.js
@@ -2,6 +2,75 @@ const { validationResult } = require('express-validator/check');
 const boardController = require('../controllers/boards');
 const { boardValidator } = require('../validators');
 
+/**
+* @swagger
+* definitions:
+*   NewBoard:
+*       properties:
+*           name:
+*               type: string
+*           privacy:
+*               type: string
+*
+* /boards/:boardId:
+*   get:
+*       tags:
+*           - Board
+*       description: Get all information of the board
+*       summary: Specific board information
+*       produces:
+*           - application/json
+*       responses:
+*           200:
+*               description: Board found sent 
+*           404:
+*               description: Board not found
+*           500:
+*               description: Internal server error
+*   put:
+*       tags:
+*           - Board
+*       description: Update lists of the specific boar
+*       summary: Update lists
+*       produces:
+*           - application/json
+*       responses:
+*           204:
+*               description: Board's lists updated 
+*           404:
+*               description: Board not found
+*           422:
+*               description: Incorrect Query
+*           500:
+*               description: Internal server error
+*
+* /boards:
+*   post:
+*       tags:
+*           - User
+*       description: Create a new empty Board
+*       summary: Create new Board
+*       produces:
+*           - application/json
+*       parameters:
+*           - name: body
+*             description: The information of the new boar
+*             in: body
+*             required: true
+*             schema:
+*               $ref: '#/definitions/NewBoard'
+*       responses:
+*           201:
+*               description: Board successfully created
+*           404:
+*               description: Board not found
+*           422:
+*               description: Invalid form data or Incorrect Query
+*           500:
+*               description: Internal server error
+
+*/
+
 module.exports = (router) => {
     router
         .get('/boards/:boardId', async (req, res) => {
@@ -37,13 +106,4 @@ module.exports = (router) => {
                 res.status(e.status).send({ error: e.message });
             }
         })
-
-        .put('/boards/:boardId/lists', async (req, res) => {
-            try {
-                await boardController.putLists(req.params.boardId, req.body.lists);
-                res.status(204).send();
-            } catch (e) {
-                res.status(e.status).send({ err: e.message });
-            }
-        });
 };
