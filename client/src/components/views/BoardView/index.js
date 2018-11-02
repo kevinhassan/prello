75105ed08456e199a6661b959fb8d63442273401
Clément Roig <clm.roig@gmail.com>
2018-11-02 15:52:13 +0100

    AddList component not DnD

--- client/src/components/views/BoardView/index.js
+++ client/src/components/views/BoardView/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
+// ===== Components
+import AddListForm from './AddListForm';
 
 // ===== Containers
 import ListComp from '../../../containers/ListComp';
@@ -13,78 +15,50 @@ import './style.css';
 
 const BoardView = props => (
     <div className="boardPanel">
-        <DragDropContext onDragEnd={props.onDragEnd}>
-            <div className="col-sm-12 boardSettingsBar">
-                <h1 className="boardSettingsBtn boardName">{props.board.name}</h1>
-                <i className="far fa-star boardSettingsBtn starBtn" />
-            </div>
-            <Droppable droppableId="currentBoard" direction="horizontal" type="LIST">
-                {dropProvided => (
-                    <div
-                        className="listsPanel"
-                        ref={dropProvided.innerRef}
-                        {...dropProvided.droppableProps}
-                    >
-                        {props.board.lists.map((l, index) => (
-                            <Draggable draggableId={l._id} index={index} key={l._id} type="LIST">
-                                {(dragProvided, dragSnapshot) => (
-                                    <div
-                                        className="listCompWrapper"
-                                        key={l._id}
-                                        ref={dragProvided.innerRef}
-                                        {...dragProvided.dragHandleProps}
-                                        {...dragProvided.draggableProps}
-                                    >
-                                        <ListComp
-                                            list={l}
-                                            isBeingDragged={dragSnapshot.isDragging}
-                                        />
-                                        {dragProvided.placeholder}
-                                    </div>
-                                )}
-                            </Draggable>
-                        ))}
-                        {dropProvided.placeholder}
-                        {props.isInputVisible ? (
+        <div className="col-sm-12 boardSettingsBar">
+            <h1 className="boardSettingsBtn boardName">{props.board.name}</h1>
+            <i className="far fa-star boardSettingsBtn starBtn" />
+        </div>
+        <div className="listsAndAddPanel">
+            <DragDropContext onDragEnd={props.onDragEnd}>
+                <Droppable droppableId="currentBoard" direction="horizontal" type="LIST">
+                    {dropProvided => (
+                        <div
+                            className="listsPanel"
+                            ref={dropProvided.innerRef}
+                            {...dropProvided.droppableProps}
+                        >
+                            {props.board.lists.map((l, index) => (
+                                <Draggable draggableId={l._id} index={index} key={l._id} type="LIST">
+                                    {(dragProvided, dragSnapshot) => (
+                                        <div
+                                            className="listCompWrapper"
+                                            key={l._id}
+                                            ref={dragProvided.innerRef}
+                                            {...dragProvided.dragHandleProps}
+                                            {...dragProvided.draggableProps}
+                                        >
+                                            <ListComp
+                                                list={l}
+                                                isBeingDragged={dragSnapshot.isDragging}
+                                            />
+                                            {dragProvided.placeholder}
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {dropProvided.placeholder}
+                        </div>
+                    )}
 
-                            <div className="appened-form">
-                                <form onSubmit={props.onListAdded}>
-                                    <input
-                                        className="form-control"
-                                        id="listName"
-                                        name="listName"
-                                        placeholder="Enter list name"
-                                        required
-                                        type="text"
-                                    />
-                                    <button className="btn btn-success addListForm-btn" type="submit">
-                                        Add list
-                                    </button>
-                                    <button
-                                        className="btn btn-secondary addListForm-btn"
-                                        type="reset"
-                                        onClick={() => props.displayAddListForm(false)}
-                                    >
-                                        <i className="fas fa-times" />
-                                    </button>
-                                </form>
-                            </div>
-
-                        ) : (
-                            <button
-                                className="btn addListButton"
-                                type="submit"
-                                onClick={() => props.displayAddListForm(true)}
-                            >
-                                <i className="fas fa-plus-circle" />
-
-                            </button>
-                        )}
-                    </div>
-                )}
-
-            </Droppable>
-        </DragDropContext>
+                </Droppable>
+            </DragDropContext>
+            <AddListForm
+                displayAddListForm={props.displayAddListForm}
+                isInputVisible={props.isInputVisible}
+                onListAdded={props.onListAdded}
+            />
+        </div>
     </div>
 );
 BoardView.propTypes = {
