37f1c1fc8475671a6e0f9473d50c1ca5465f3dd6
clement <cleloubiere@gmail.com>
2018-11-07 18:55:13 +0100

    add : slack controller and slack route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 require('./boards')(router);
 require('./cards')(router);
 require('./lists')(router);
+require('./slack')(router);
 require('./users')(router);
 require('./teams')(router);
 require('./users')(router);
