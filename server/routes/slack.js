c1a23be03f340c8efd817ac3ee0e6a5c93a6ae5d
clement <cleloubiere@gmail.com>
2018-11-08 16:22:37 +0100

    add : listsInfo et cardsInfo routes

--- server/routes/slack.js
+++ server/routes/slack.js
@@ -22,5 +22,53 @@ module.exports = (router) => {
             } catch (e) {
                 res.status(e.status).send({ error: e.message });
             }
+        })
+        .post('/slack/lists', async (req, res) => {
+            try {
+                let listTitle = ' ';
+                res.status(200).send('Fetching...');
+                const board = await slackController.getBoard();
+                board.lists.forEach((list) => {
+                    listTitle += ` ${list.name}`;
+                });
+                axios({
+                    url: req.param('response_url'),
+                    headers: { 'Content-Type': 'application/json' },
+                    data: {
+                        attachments: [{
+                            color: '#009cdd',
+                            text: `The lists of the board ${board.name} are ${listTitle}.`
+                        }]
+                    },
+                    method: 'post',
+                }).then().catch();
+            } catch (e) {
+                res.status(e.status).send({ error: e.message });
+            }
+        })
+        .post('/slack/cards', async (req, res) => {
+            try {
+                let cardTitle = ' ';
+                res.status(200).send('Fetching...');
+                const board = await slackController.getBoard();
+                board.lists.forEach((list) => {
+                    list.cards.forEach((card) => {
+                        cardTitle += ` ${card.name}`;
+                    });
+                });
+                axios({
+                    url: req.param('response_url'),
+                    headers: { 'Content-Type': 'application/json' },
+                    data: {
+                        attachments: [{
+                            color: '#009cdd',
+                            text: `The card of the board ${board.name} are ${cardTitle}.`
+                        }]
+                    },
+                    method: 'post',
+                }).then().catch();
+            } catch (e) {
+                res.status(e.status).send({ error: e.message });
+            }
         });
 };
