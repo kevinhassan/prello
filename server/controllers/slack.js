9dd26c42b28045383e97fa9cefb8290553431244
clement <cleloubiere@gmail.com>
2018-11-09 15:59:15 +0100

    better parser with multiple user or cards

--- server/controllers/slack.js
+++ server/controllers/slack.js
@@ -3,19 +3,20 @@ const BoardController = require('./boards');
 
 function parse(stringToParse) {
     const retour = {
-        /* board: '',
-        card: '',
+        board: '',
+        cards: [],
         keyword: '',
-        list: '',
-        user: '', */
+        lists: [],
+        users: []
     };
-    const params = stringToParse.split(' ');
+    const params = stringToParse.split(/(?=&|@|\$|%)/g);
     params.forEach((param) => {
-        if (param.charAt(0) === '#')retour.board = param.substring(1);
-        else if (param.charAt(0) === '$')retour.card.push(param.substring(1));
-        else if (param.charAt(0) === '%')retour.list.push(param.substring(1));
-        else if (param.charAt(0) === '@')retour.user.push(param.substring(1));
-        else if (param === params[0]) retour.keyword = param;
+        console.log(param);
+        if (param.charAt(0) === '#')retour.board = param.substring(1).trim();
+        else if (param.charAt(0) === '$')retour.cards.push(param.substring(1).trim());
+        else if (param.charAt(0) === '%')retour.lists.push(param.substring(1).trim());
+        else if (param.charAt(0) === '@')retour.users.push(param.substring(1).trim());
+        else if (param === params[0]) retour.keyword = param.trim();
     });
     return retour;
 }
@@ -27,7 +28,12 @@ exports.slackAction = async (stringToParse) => {
     try {
         const params = parse(stringToParse);
         const message = (params) => {
-            if (params.keyword === 'add') return 'you have choose add action';
+            if (params.keyword === 'add') {
+                if (params.users.length !== 0 && params.cards.length !== 0) {
+                    return `you have add ${params.users.toString()} to ${params.cards.toString()}`;
+                }
+                return 'you have to choose a user';
+            }
             if (params.keyword === 'duedate') return 'you have choose duedate action';
             if (params.keyword === 'remove') return 'you have choose remove action';
             return 'no valid key-word found. \nValid key-word are add, remove, due date ';
