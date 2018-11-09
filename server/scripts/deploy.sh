266fda0f7680820486d6cb90bda705705dd0ea41
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2018-11-09 16:05:29 +0100

    fix: npm seed package json
    Signed-off-by: Kévin Hassan <kevin.hassan@etu.umontpellier.fr>

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -3,9 +3,7 @@ if [ $# -gt 0 ]
 then ROOTDIR=$1 && cd $ROOTDIR
 fi
 GIT=`which git`
-EGREP= `which egrep`
-WC=`which wc`
-${GIT} add -a .
+${GIT} add -A .
 ${GIT} commit -m "deploy react app #"
 ${GIT} push -f dokku master
 echo "React app successfully deployed"
