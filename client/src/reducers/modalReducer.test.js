c85c591d732634d0671295532c4dd997de001eb3
Clément Roig <clm.roig@gmail.com>
2018-11-10 20:39:46 +0100

    Redux tests: 100% coverage on modalReducer + remove index.js from tests

--- client/src/reducers/modalReducer.test.js
+++ client/src/reducers/modalReducer.test.js
@@ -10,18 +10,56 @@ describe('Action not referenced', () => {
     });
 });
 
-describe('modal/DISPLAY_LOADING_MODAL', () => {
+describe(actions.DISPLAY_LOADING_MODAL, () => {
     it('should set isModalOpen to true', () => {
         const action = actions.displayLoadingModalAction();
         const finalState = modalReducer({}, action);
+
         expect(finalState.isLoadingModalOpen).toEqual(true);
     });
 });
 
-describe('modal/HIDE_LOADING_MODAL', () => {
+describe(actions.HIDE_LOADING_MODAL, () => {
     it('should set isModalOpen to false', () => {
         const action = actions.hideLoadingModalAction();
         const finalState = modalReducer({}, action);
+
         expect(finalState.isLoadingModalOpen).toEqual(false);
+    });
+});
+
+describe(actions.DISPLAY_ERROR_MESSAGE_MODAL, () => {
+    it('should set an error message', () => {
+        const action = actions.displayErrorMessageAction('an error occured');
+        const finalState = modalReducer({}, action);
+
+        expect(finalState.errorMessage).toEqual('an error occured');
+    });
+});
+
+describe(actions.HIDE_ERROR_MESSAGE_MODAL, () => {
+    it('should reset the error message', () => {
+        const action = actions.hideErrorMessageAction();
+        const finalState = modalReducer({}, action);
+
+        expect(finalState.errorMessage).toEqual('');
+    });
+});
+
+describe(actions.DISPLAY_SUCCESS_MESSAGE_MODAL, () => {
+    it('should set a success message', () => {
+        const action = actions.displaySuccessMessageAction('Testing success message modal');
+        const finalState = modalReducer({}, action);
+
+        expect(finalState.successMessage).toEqual('Testing success message modal');
+    });
+});
+
+describe(actions.HIDE_SUCCESS_MESSAGE_MODAL, () => {
+    it('should reset the success message', () => {
+        const action = actions.hideSuccessMessageAction();
+        const finalState = modalReducer({}, action);
+
+        expect(finalState.successMessage).toEqual('');
     });
 });
