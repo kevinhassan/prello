3acb53144ab06acfb2d711c0baae860bb85b90d2
Clément Roig <clm.roig@gmail.com>
2018-11-10 19:50:32 +0100

    Archive card + labels css

--- client/src/components/views/CardDetailView/index.js
+++ client/src/components/views/CardDetailView/index.js
@@ -11,79 +11,93 @@ import './style.css';
 
 const CardDetailView = props => (
     <div className="cardDetailOverlay" onMouseDown={props.closeCardDetail}>
-        <div className="cardDetailContent" key={props.card._id}>
-            <div className="">
-                <i className="fas fa-times closeCardDetailIcon" onClick={props.closeCardDetail} onKeyDown={props.closeCardDetail} />
+        <div className="cardDetailContent container-fluid" key={props.card._id}>
 
-                <h2 className="cardDetail-h2 cardDetailTitle">
-                    {props.card.name}
-                </h2>
-                <p>
+            <div className="row">
+                <div className="col-sm-12">
+                    <i className="fas fa-times closeCardDetailIcon" onClick={props.closeCardDetail} onKeyDown={props.closeCardDetail} />
+                    <h2 className="cardDetail-h2 cardDetailTitle">
+                        {props.card.name}
+                    </h2>
+                </div>
+            </div>
+            <div className="row">
+                <p className="col-sm-12" style={{ margin: 0 }}>
                     in
                     {' '}
                     <span className="cardDetailInList">{props.card.list.name}</span>
                 </p>
+            </div>
 
-                <div className="row">
+            <div className="row" style={{ marginTop: '5px' }}>
+                <div className="col-sm-12 cardDetailActions">
+                    <button type="button" className="archiveBtn btn btn-secondary" onClick={props.archiveCard}>
+                        <i className="fas fa-archive" />
+                        {' '}
+                        Archive
+                    </button>
+                    <button type="button" className="watchBtn btn btn-secondary">
+                        <i className="fas fa-eye" />
+                        {' '}
+                        Watch
+                    </button>
+                    <hr />
+                </div>
+            </div>
 
-                    {/* ===== DUE DATE ===== */}
-                    <div className="col-sm-4">
-                        <h2 className="cardDetail-h2">
-                            <i className="fas fa-calendar-alt" />
-                            {' '}
+            <div className="row">
+
+                {/* ===== DUE DATE ===== */}
+                <div className="col-sm-4">
+                    <h2 className="cardDetail-h2">
+                        <i className="fas fa-calendar-alt" />
+                        {' '}
                             Due date
-                        </h2>
+                    </h2>
 
-                        <div className="cardElementContent">
-                            {props.card.dueDate
-                                ? (
-                                    <p className="cardDetailDate">{new Date(props.card.dueDate).toDateString()}</p>
-                                )
-                                : (
-                                    <button
-                                        className="btn btn-link btn-addElement"
-                                        type="button"
-                                    >
+                    <div className="cardElementContent">
+                        {props.card.dueDate
+                            ? (
+                                <p className="cardDetailDate">{new Date(props.card.dueDate).toDateString()}</p>
+                            )
+                            : (
+                                <button
+                                    className="btn btn-link btn-addElement"
+                                    type="button"
+                                >
                                 Add a due date...
-                                    </button>
-                                ) }
-                        </div>
-
+                                </button>
+                            ) }
                     </div>
-                    {/* ==================== */}
-
-                    {/* ===== LABELS ===== */}
-                    <div className="col-sm-8">
-                        <Labels
-                            boardLabels={props.boardLabels}
-                            cardId={props.card._id}
-                            changeIsEditingLabels={props.changeIsEditingLabels}
-                            isEditingLabels={props.isEditingLabels}
-                            labels={props.card.labels}
-                            deleteLabel={props.deleteLabel}
-                        />
-                    </div>
-                    {/* ==================== */}
 
                 </div>
-
-                {/* ===== DESCRIPTION ===== */}
-                <Description
-                    changeIsEditingDescription={props.changeIsEditingDescription}
-                    description={props.card.description}
-                    editDescription={props.editDescription}
-                    isEditingDescription={props.isEditingDescription}
-                />
                 {/* ==================== */}
 
-
-                <button type="button" className="archiveBtn btn btn-primary" onClick={props.archiveCard}>
-                    <i className="fas fa-archive" />
-                    {' '}
-                    Archive card
-                </button>
+                {/* ===== LABELS ===== */}
+                <div className="col-sm-8">
+                    <Labels
+                        boardLabels={props.boardLabels}
+                        cardId={props.card._id}
+                        changeIsEditingLabels={props.changeIsEditingLabels}
+                        isEditingLabels={props.isEditingLabels}
+                        labels={props.card.labels}
+                        deleteLabel={props.deleteLabel}
+                    />
+                </div>
+                {/* ==================== */}
 
             </div>
+
+            {/* ===== DESCRIPTION ===== */}
+            <Description
+                changeIsEditingDescription={props.changeIsEditingDescription}
+                description={props.card.description}
+                editDescription={props.editDescription}
+                isEditingDescription={props.isEditingDescription}
+            />
+            {/* ==================== */}
+
+
         </div>
     </div>
 );
