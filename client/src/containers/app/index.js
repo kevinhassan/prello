7210db2d40b5ab6f70ae598baa89217be12ba1e3
Clément Roig <clm.roig@gmail.com>
2018-11-11 18:17:54 +0100

    Fix develop rebase

--- client/src/containers/app/index.js
+++ client/src/containers/app/index.js
@@ -1,40 +1,36 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-            <PrivateRoute authed={isAuthenticated()} exact path="/boards/:boardId" component={BoardComp} />
-            <PrivateRoute authed={isAuthenticated()} exact path="/boards" component={BoardsComp} />
-            <PrivateRoute authed={isAuthenticated()} exact path="/profile" component={ProfileComp} />
-        </main>
-    </div>
-);
+import GraphicalCharter from '../GraphicalCharter';
+import BoardComp from '../BoardComp';
+import BoardsComp from '../BoardsComp';
+import HomeComp from '../HomeComp';
+import Modal from '../../components/modal';
+import Header from '../header';
+import MemberComp from '../MemberComp';
+import ProfileComp from '../ProfileComp';
+import RegisterComp from '../RegisterComp';
+import SignInComp from '../SignInComp';
 
-export default App;
+import PrivateRoute from '../../components/PrivateRoute';
+import Page404 from '../../components/Page404';
 
-            <PrivateRoute authed={isAuthenticated()} exact path="/boards/:boardId" component={BoardComp} />
-            <PrivateRoute authed={isAuthenticated()} exact path="/boards" component={BoardsComp} />
-            <PrivateRoute authed={isAuthenticated()} exact path="/profile" component={ProfileComp} />
-        </main>
-    </div>
-);
+import './style.css';
 
-export default App;
+const isAuthenticated = () => localStorage.getItem('prello_token') !== null;
 
-            <PrivateRoute authed={isAuthenticated()} exact path="/boards/:boardId" component={BoardComp} />
-            <PrivateRoute authed={isAuthenticated()} exact path="/boards" component={BoardsComp} />
-            <PrivateRoute authed={isAuthenticated()} exact path="/profile" component={ProfileComp} />
-        </main>
-    </div>
-);
-
-export default App;
-
+const App = () => (
+    <div className="appContainer">
+        <Header />
+        <Modal />
+        <main>
             <Switch>
                 <Route exact path="/" component={HomeComp} />
                 <Route exact path="/graphical-charter" component={GraphicalCharter} />
+                <Route exact path="/members/:memberId" component={MemberComp} />
                 <Route exact path="/register" component={RegisterComp} />
                 <Route exact path="/signin" component={SignInComp} />
-            <Route exact path="/members/:memberId" component={MemberComp} />
-            <PrivateRoute authed={isAuthenticated()} exact path="/boards/:boardId" component={BoardComp} />
-            <PrivateRoute authed={isAuthenticated()} exact path="/boards" component={BoardsComp} />
-            <PrivateRoute authed={isAuthenticated()} exact path="/profile" component={ProfileComp} />
+
                 <PrivateRoute authed={isAuthenticated()} exact path="/boards/:boardId" component={BoardComp} />
                 <PrivateRoute authed={isAuthenticated()} exact path="/boards" component={BoardsComp} />
                 <PrivateRoute authed={isAuthenticated()} exact path="/profile" component={ProfileComp} />
