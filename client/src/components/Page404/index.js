535912c0728d1071c9c7958d2c7b37557e025f64
Clément Roig <clm.roig@gmail.com>
2018-11-11 18:22:05 +0100

    Better 404 (logged in or not ?)

--- client/src/components/Page404/index.js
+++ client/src/components/Page404/index.js
@@ -14,12 +14,19 @@ const Page404 = () => (
         </h1>
         <div className="text-center" style={{ fontSize: '1.4rem' }}>
             <p className="text-center">Sorry, we can not find the page you are looking for...</p>
-            <p className="text-center">Maybe you would to go somewhere else:</p>
+            <p className="text-center">Maybe you would like to go somewhere else:</p>
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/register">Register</Link></li>
-                <li><Link to="/signin">Sign in</Link></li>
-                <li><Link to="/boards">Your boards</Link></li>
+                {localStorage.getItem('prello_token') === null
+                    ? (
+                        <div>
+                            <li><Link to="/register">Register</Link></li>
+                            <li><Link to="/signin">Sign in</Link></li>
+                        </div>
+                    ) : (
+                        <li><Link to="/boards">Your boards</Link></li>
+                    )
+                }
             </ul>
         </div>
     </div>
