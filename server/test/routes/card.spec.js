b5acab6754bc0b31870096f78cda1245a4b46bdc
Cyprien Legrand <cyprien_legrand@hotmail.fr>
2018-11-12 16:52:10 +0100

    front & back tests

--- server/test/routes/card.spec.js
+++ server/test/routes/card.spec.js
@@ -213,3 +213,18 @@ describe('PUT /cards/:cardId/archive', () => {
             .expect(404, done);
     });
 });
+
+describe('PUT /cards/:cardId/dueDate', () => {
+    it('should return 200 OK', (done) => {
+        request(app)
+            .put(`/cards/${cardData.id}/dueDate`)
+            .set('Authorization', `Bearer ${tokenMember}`)
+            .expect(204, done);
+    });
+    it('should return 404 ERROR', (done) => {
+        request(app)
+            .put('/cards/123456/dueDate/')
+            .set('Authorization', `Bearer ${tokenMember}`)
+            .expect(404, done);
+    });
+});
