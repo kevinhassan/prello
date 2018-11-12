e07ebda389f48bf2555212003a709354a95dc67e
Clément Roig <clm.roig@gmail.com>
2018-11-12 12:07:18 +0100

    Fix middleware

--- server/middlewares/team.js
+++ server/middlewares/team.js
@@ -61,17 +61,21 @@ const canSee = async (req, res, next) => {
             // Is the team visible ?
             if (!team.isVisible) throw new MyError(401, 'You are not allowed to access this team. Please sign in and try again.');
             else next();
-        }
-
+        } else {
         // Is the user Admin or member ?
-        const admin = team.admins.find(admin => req.user._id.toString() === admin._id.toString());
-        const member = team.members.find(member => req.user._id.toString() === member._id.toString());
-        if (member || admin) next();
-
-        // Is the team visible ?
-        if (!team.isVisible) throw new MyError(403, 'You can\'t access this team because it is not visible.');
-        next();
+            const admin = team.admins.find(admin => req.user._id.toString() === admin._id.toString());
+            const member = team.members.find(member => req.user._id.toString() === member._id.toString());
+            if (member || admin) next();
+            else {
+            // Is the team visible ?
+                if (!team.isVisible) {
+                    throw new MyError(403, 'You can\'t access this team because it is not visible.');
+                }
+                next();
+            }
+        }
     } catch (e) {
+        console.log(e);
         if (e.name === 'CastError') {
             return res.status(404).send({ error: 'Team not found' });
         } if (e.status) {
