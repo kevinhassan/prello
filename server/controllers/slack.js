5d680e3d3fed7f1cd0ead8d6251bf1860b38456d
cle loubiere <cleloubiere@gmail.com>
2018-11-12 19:48:32 +0100

    WIP ajout de liste

--- server/controllers/slack.js
+++ server/controllers/slack.js
@@ -8,17 +8,18 @@ function parse(stringToParse) {
         board: '',
         cards: [],
         keyword: '',
-        lists: [],
+        list: '',
         users: []
     };
+    console.log(stringToParse);
     const params = stringToParse.split(/(?=&|@|\$|%)/g);
     params.forEach((param) => {
         console.log(param);
         if (param.charAt(0) === '#')retour.board = param.substring(1).trim();
         else if (param.charAt(0) === '$')retour.cards.push(param.substring(1).trim());
-        else if (param.charAt(0) === '%')retour.lists.push(param.substring(1).trim());
+        else if (param.charAt(0) === '%')retour.list = (param.substring(1).trim());
         else if (param.charAt(0) === '@')retour.users.push(param.substring(1).trim());
-        else if (param === params[0]) retour.keyword = param.trim();
+        else if (param === params[0]) retour.keyword = param.trim().split(' ');
     });
     return retour;
 }
@@ -28,29 +29,40 @@ function parse(stringToParse) {
  */
 exports.slackAction = async (stringToParse) => {
     try {
-        const board = await BoardController.getBoard('b00000000001');
-        const list = board.lists[0];
         const params = parse(stringToParse);
-        const message = (params) => {
-            if (params.keyword === 'add') {
+        const board = await BoardController.getBoard('b00000000001');
+        const listParam = board.lists.find(list => list.name === params.list);
+        const message = async (params) => {
+            if (params.keyword[0] === 'add') {
                 if (params.users.length !== 0 && params.cards.length !== 0) {
                     return `you have add ${params.users.toString()} to ${params.cards.toString()}`;
                 }
                 if (params.cards.length !== 0) {
-                    params.cards.forEach(async (card) => {
-                        CardController.postCard({name:card, list:list.id});
+                    if (!listParam && !params.keyword.includes('-R')) {
+                        return `the list ${params.list} does not exist`;
+                    }
+                    const list = async (listParam) => {
+                        if (!listParam) {
+                            const boar = (await BoardController.postList(board.id, params.list).lists); //.find(list => list.name === params.list);
+                            return boar[boar.length];
+                        }
+                        return listParam;
+                    };
+                    console.log(list());
+                    await params.cards.forEach(async (card) => {
+                        await CardController.postCard({ name: card, list: await list().id });
                     });
-                    return `you have add the cards ${params.cards.toString()} to ${list.name}`;
+                    return `you have add the cards ${params.cards.toString()} to ${params.list}`;
                 }
                 return 'you have to choose a user';
             }
-            if (params.keyword === 'duedate') {
+            if (params.keyword[0] === 'duedate') {
                 return 'the next task to do are : ';
             }
-            if (params.keyword === 'remove') return 'you have choose remove action';
+            if (params.keyword[0] === 'remove') return 'you have choose remove action';
             return 'no valid key-word found. \nValid key-word are add, remove, due date ';
         };
-        return message(params);
+        return await message(params);
     } catch (err) {
         if (err.status) throw err;
         else if (err.name === 'CastError') {
