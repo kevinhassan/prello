f3f42c6990e10a4a87acfd1d7f98205220143d49
Clément Roig <clm.roig@gmail.com>
2018-11-13 19:00:48 +0100

    Middleware for get board (check all rights)

--- server/middlewares/board.js
+++ server/middlewares/board.js
@@ -48,4 +48,55 @@ const isAdmin = async (req, res, next) => {
     }
 };
 
-module.exports = { isMember, isAdmin };
+/**
+ * Check if the user can see the board.
+ */
+const canSee = async (req, res, next) => {
+    try {
+        const board = await Board.findById(req.params.boardId).select('members teams').populate([
+            {
+                path: 'teams',
+                select: 'members',
+                populate: {
+                    path: 'members',
+                    select: '_id'
+                }
+            }, {
+                path: 'members',
+                select: '_id'
+            }
+        ]);
+
+        if (!board) throw new MyError(404, 'Board not found');
+
+        // User not logged in
+        if (!req.user) {
+            // Is the board public ?
+            if (!board.visibility === 'public') throw new MyError(401, 'You are not allowed to access this team. Please sign in and try again.');
+            else next();
+        } else {
+            // Is the user a board member ?
+            let member = board.members.find(member => req.user._id.toString() === member._id.toString());
+            if (member) next();
+
+            else {
+                // Is the user a team member and board not private ?
+                member = board.teams.members.find(member => req.user._id.toString() === member._id.toString());
+                if (member && !board.visibility === 'private') {
+                    next();
+                } else {
+                    throw new MyError(403, 'You can\'t access this board.');
+                }
+            }
+        }
+    } catch (e) {
+        if (e.name === 'CastError') {
+            return res.status(404).send({ error: 'Board not found' });
+        } if (e.status) {
+            return res.status(e.status).send({ error: e.message });
+        }
+        return res.status(500).send({ error: 'Internal server error' });
+    }
+};
+
+module.exports = { canSee, isMember, isAdmin };
