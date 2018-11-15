850f96c7adb55322f9d3c0e0fe6a5e76e3b806fa
Clément Roig <clm.roig@gmail.com>
2018-11-15 02:05:22 +0100

    Working on link github <=> board

--- client/src/components/views/BoardView/GithubLinkBoard/index.js
+++ client/src/components/views/BoardView/GithubLinkBoard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 // ===== Others
@@ -8,28 +8,54 @@ import './style.css';
 
 const GithubLinkBoard = props => (
     <span style={{ display: 'inline-block' }}>
-        <select defaultValue="" className="custom-select" id="repoUrl" required>
-            <option value="" disabled>
-                Link to
-                {' '}
-                <i className="fab fa-github" />
-            </option>
-            <option value="public">
-                                Public
-            </option>
-            <option value="team">
-                                Team
-            </option>
-            <option value="private">
-                                Private
-            </option>
-        </select>
+        {console.log(props)}
+        {props.boardGithubRepo && !props.isReposListVisible
+            ? (
+                <Fragment>
+                    <a href={props.boardGithubRepo.url} style={{ color: '#222' }} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-github" />
+                        {' '}
+                        {props.boardGithubRepo.name}
+                    </a>
+                    {' '}
+                    <button
+                        className="btn btn-primary btn-sm"
+                        type="submit"
+                        onClick={() => props.displayReposList(true)}
+                        style={{ transform: 'scale(0.9)' }}
+                    >
+                        <i className="fas fa-pen fa-xs" />
+                    </button>
+                </Fragment>
+            ) : (
+                <span style={{ display: 'flex' }}>
+                    <select onChange={event => props.linkBoardToGithubRepo(event)} defaultValue="" className="custom-select custom-select-sm" id="repoUrl" required>
+                        <option value="" disabled>
+                    Link to Github
+                        </option>
+                        {props.repos.filter(repo => !repo.private).map(repo => (
+                            <option key={repo.url} value={JSON.stringify(repo)}>{repo.name}</option>
+                        ))}
+                    </select>
+                    <button
+                        className="btn btn-secondary btn-sm"
+                        type="submit"
+                        onClick={() => props.displayReposList(false)}
+                    >
+                        <i className="fas fa-times" />
+                    </button>
+                </span>
+
+            )}
     </span>
 );
 
 GithubLinkBoard.propTypes = {
     repos: PropTypes.arrayOf(PropTypes.object).isRequired,
+    isReposListVisible: PropTypes.bool.isRequired,
+    displayReposList: PropTypes.func.isRequired,
     linkBoardToGithubRepo: PropTypes.func.isRequired,
+    boardGithubRepo: PropTypes.object,
 };
 
 export default GithubLinkBoard;
--- client/src/containers/BoardComp/GithubLinkBoardComp/index.js
+++ client/src/containers/BoardComp/GithubLinkBoardComp/index.js
@@ -13,43 +13,71 @@ import GithubLinkBoard from '../../../components/views/BoardView/GithubLinkBoard
 class GithubLinkBoardComp extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isReposListVisible: false,
+            repos: [{
+                name: 'repo1',
+                private: true,
+                url: 'http://facebook.fr',
+            }, {
+                name: 'Prello',
+                private: false,
+                url: 'http://amazon.fr',
+            }, {
+                name: 'Data Science',
+                private: false,
+                url: 'http://ebay.fr',
+            }],
+            boardGithubRepo: {
+                name: 'Awesome Repo',
+                private: false,
+                url: 'http://google.fr',
+            },
+        };
         this.linkBoardToGithubRepo = this.linkBoardToGithubRepo.bind(this);
+        this.displayReposList = this.displayReposList.bind(this);
     }
 
     componentWillMount() {
-        // TODO : fetch repos and put them in the state
+        // TODO : fetch repos
+    }
+
+    displayReposList(value) {
+        this.setState({ isReposListVisible: value });
     }
 
     linkBoardToGithubRepo(event) {
         event.preventDefault();
-        const repoUrl = event.target.repoUrl.value;
+        const newRepo = JSON.parse(event.target.value);
     }
 
     render() {
-        if (this.props.repos) {
-            if (this.props.repos.length > 0) {
-                return (
-                    <GithubLinkBoard
-                        repos={this.props.repos}
-                        linkBoardToGithubRepo={this.linkBoardToGithubRepo}
-                    />
-                );
-            }
-        }
-        return '';
+        return (
+            <GithubLinkBoard
+                repos={this.state.repos}
+                linkBoardToGithubRepo={this.linkBoardToGithubRepo}
+                boardGithubRepo={this.state.boardGithubRepo}
+                isReposListVisible={this.state.isReposListVisible}
+                displayReposList={this.displayReposList}
+            />
+        );
     }
 }
 GithubLinkBoardComp.propTypes = {
     boardId: PropTypes.string.isRequired,
+    boardGithubRepo: PropTypes.object,
     linkBoardToRepoGithub: PropTypes.func.isRequired,
     repos: PropTypes.arrayOf(PropTypes.object),
 };
 GithubLinkBoardComp.defaultProps = {
     repos: undefined,
+    boardGithubRepo: undefined,
 };
 
 // Put info from the store state in props
-const mapStateToProps = () => {};
+const mapStateToProps = ({ currentBoard }) => ({
+    boardGithubRepo: currentBoard.githubRepo,
+});
 
 // Put actions in props
 const mapDispatchToProps = dispatch => bindActionCreators(
