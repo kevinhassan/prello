b3934e69634745152a47787439d29a51597f789f
cleloubiere <cleloubiere@gmail.com>
2018-11-15 13:06:11 +0100

    change treatment of request from controller to router

--- server/routes/slack.js
+++ server/routes/slack.js
@@ -1,13 +1,80 @@
 const axios = require('axios');
 const slackController = require('../controllers/slack');
+const BoardController = require('../controllers/boards');
+const CardController = require('../controllers/cards');
 
+function parse(stringToParse) {
+    const retour = {
+        board: '',
+        cards: [],
+        keyword: '',
+        list: '',
+        users: []
+    };
+    console.log(stringToParse);
+    const params = stringToParse.split(/(?= [&@$%].*)/g);
+    params.forEach((param) => {
+        const trimedParam = param.trim();
+        if (trimedParam.charAt(0) === '#')retour.board = trimedParam.substring(1);
+        else if (trimedParam.charAt(0) === '$')retour.cards.push(trimedParam.substring(1));
+        else if (trimedParam.charAt(0) === '%')retour.list = (trimedParam.substring(1));
+        else if (trimedParam.charAt(0) === '@')retour.users.push(trimedParam.substring(1));
+        else if (trimedParam === params[0]) retour.keyword = trimedParam.split(' ');
+    });
+    return retour;
+}
 
 module.exports = (router) => {
     router
         .post('/slack/test', async (req, res) => {
             try {
                 res.status(204).send();
-                const result = await slackController.slackAction(req.param('text'));
+                // const result = await slackController.slackAction(req.param('text'));
+                const params = parse(req.param('text'));
+                const board = await BoardController.getBoard('b00000000001');
+                const listParam = board.lists.find(list => list.name === params.list && list.isArchived === false);
+                const message = async (params) => {
+                    if (params.keyword[0] === 'add') {
+                        if (params.users.length !== 0 && params.cards.length !== 0) {
+                            return `You have added ${params.users.toString()} to ${params.cards.toString()}.`;
+                        }
+                        if (params.cards.length !== 0) {
+                            if (!listParam && !params.keyword.includes('-R')) {
+                                return `The list ${params.list} does not exist.`;
+                            }
+                            if (!listParam) {
+                                const list = (await BoardController.postList(board.id, params.list));
+                                params.cards.forEach(async (card) => {
+                                    await CardController.postCard({ name: card, list: list._id });
+                                });
+                                return `You have added the cards ${params.cards.toString()} to the new list ${params.list}.`;
+                            }
+                            params.cards.forEach(async (card) => {
+                                await CardController.postCard({ name: card, list: listParam._id });
+                            });
+                            return `You have add the cards ${params.cards.toString()} to ${params.list}.`;
+                        }
+                        return 'You have to choose a user.';
+                    }
+                    if (params.keyword[0] === 'duedate') {
+                        return 'The next task to do are : .';
+                    }
+                    if (params.keyword[0] === 'remove') {
+                        if (!listParam) {
+                            return `The list ${params.list} does not exist.`;
+                        }
+                        await params.cards.forEach(async (card) => {
+                            const findCard = await listParam.cards.filter(listCard => listCard.name === card);
+                            findCard.forEach((cardToArchive) => {
+                                CardController.archiveCard(cardToArchive._id);
+                            });
+                        });
+                        return `You have archive the cards ${params.cards.toString()}.`;
+                    }
+                    return 'No valid key-word found. \nValid key-word are add, remove, duedate.';
+                };
+                const result = await message(params);
+
                 axios({
                     url: req.param('response_url'),
                     headers: { 'Content-Type': 'application/json' },
