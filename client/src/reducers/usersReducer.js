cce8199f66fbea91af54cd0f4a9e40edc6aa992a
Kévin Hassan <kevin.hassan@etu.umontpellier.fr>
2018-11-15 12:24:23 +0100

    fix: tests reducer team
    Signed-off-by: Kévin Hassan <kevin.hassan@etu.umontpellier.fr>

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -44,12 +44,12 @@ export default function usersReducer(state = initialState, action) {
             };
         }
         case teamActions.DELETE_TEAM_SUCCESS: {
-            const index = state.profile.teams.indexOf(action.payload.team);
-            state.profile.teams.splice(index, 1);
+            const teamToRemove = action.payload.team;
             return {
                 ...state,
                 profile: {
                     ...state.profile,
+                    teams: state.profile.teams.filter(team => team.id !== teamToRemove.id),
                 },
             };
         }
