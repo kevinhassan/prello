73b34dfa6040ef07244e98815e9879e6afe86fd4
Clément Roig <clm.roig@gmail.com>
2018-11-15 02:05:22 +0100

    Working on link github <=> board

--- client/src/models/Board.js
+++ client/src/models/Board.js
@@ -1,10 +1,11 @@
 export default function Board({
-    _id, isArchived, name, labels, lists, visibility, teams, members, owner,
+    _id, isArchived, name, labels, lists, visibility, teams, members, owner, boardGithubRepo,
 } = {}) {
     this._id = _id; // String
     this.isArchived = isArchived; // Boolean
     this.name = name; // String
     this.visibility = visibility; // String
+    this.boardGithubRepo = boardGithubRepo; // Object {name, url, private} : {String, String, Boolean}
     this.labels = labels; // Array(Label)
     this.lists = lists; // Array(List)
     this.members = members; // Array({User, isAdmin: bool})
