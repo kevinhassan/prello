7d4764498a8adf2873e0222736fb4bc5ca0beebe
Cyprien Legrand <cyprien_legrand@hotmail.fr>
2018-11-16 10:11:48 +0100

    fix CSS + unarchive card button

--- client/src/components/views/BoardView/BoardArchivedListsView/index.js
+++ client/src/components/views/BoardView/BoardArchivedListsView/index.js
@@ -4,32 +4,32 @@ import PropTypes from 'prop-types';
 // ===== Components
 
 // ===== Others
-
+import './style.css';
 // ==================================
 
 
-const BoardArchivedItemsView = props => (
+const BoardArchivedListsView = props => (
     <span className="boardSettingsItem" style={{ display: 'inline-block' }}>
-        <i className="fas fa-save" />
-        <select name="Archived lists">
+        <select name="Archived lists" className="custom-select">
             <option disabled selected>Archived lists</option>
             {props.board.lists.filter(list => list.isArchived).length > 0
                 ? (
                     props.board.lists.filter(list => list.isArchived).map(list => (
                         <option name={list._id} onClick={() => props.unarchiveList(list)}>{list.name}</option>))
                 ) : (
-                    <span>0 archived list</span>
+                    <span>No archived list</span>
                 )
             }
+            {}
         </select>
     </span>
 );
 
-BoardArchivedItemsView.propTypes = {
+BoardArchivedListsView.propTypes = {
     board: PropTypes.object.isRequired,
     unarchiveList: PropTypes.func.isRequired,
 };
-BoardArchivedItemsView.defaultProps = {
+BoardArchivedListsView.defaultProps = {
 };
 
-export default BoardArchivedItemsView;
+export default BoardArchivedListsView;
